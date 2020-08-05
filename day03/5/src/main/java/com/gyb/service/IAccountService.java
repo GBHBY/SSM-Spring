d7fcad9d60698273e81a4340d7bf1e04ac3a6fe0
GBHBY <15776547151@163.com>
2020-08-05 11:33:22 +0800

    SpringAOP注解

---
+++ day03/5/src/main/java/com/gyb/service/IAccountService.java
@@ -0,0 +1,15 @@
+package com.gyb.service;
+
+/**
+ * @author gyb
+ * 程序的业务层接口
+ */
+public interface IAccountService {
+
+    void save();
+
+    void updata(int i);
+
+    int deleteAccount();
+
+}
