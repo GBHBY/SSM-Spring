3c6724ab37628c23447a8f2c09b5418f5905e89a
GBHBY <15776547151@163.com>
2020-08-15 22:30:06 +0800

    基于xml的声明式事务控制
    看代码

---
+++ day04/5/src/main/java/com/gyb/service/impl/AccountServiceImpl.java
@@ -0,0 +1,40 @@
+package com.gyb.service.impl;
+
+import com.gyb.dao.AccountDao;
+import com.gyb.domain.Account;
+import com.gyb.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author gb
+ * @Date 2020/7/5 13:42
+ * @Version 1.0
+ */
+@Service("accountServiceImpl")
+public class AccountServiceImpl implements AccountService {
+
+    @Autowired
+    private AccountDao accountDao;
+
+    public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }
+
+
+    @Override
+    public Account findAccountById(Integer accountId) {
+        return accountDao.findAccountById(accountId);
+    }
+
+    @Override
+    public void transfer(String sourceName, String targetName, Float money) {
+        Account sourceAccount = accountDao.findAccountByName(sourceName);
+        sourceAccount.setMoney(sourceAccount.getMoney() - money);
+        accountDao.updateAccount(sourceAccount);
+        int i = 1/0;
+        Account targetAccount = accountDao.findAccountByName(targetName);
+        targetAccount.setMoney(targetAccount.getMoney() + money);
+        accountDao.updateAccount(targetAccount);
+    }
+}
