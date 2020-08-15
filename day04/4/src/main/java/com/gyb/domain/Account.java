7265bb7d60c66e2c1e7b8f158eeeba528ccb82fa
GBHBY <15776547151@163.com>
2020-08-15 22:18:39 +0800

    有问题不知道干嘛的
    readme文件写的是spring自带的事务控制，但是没有相关代码

---
+++ day04/4/src/main/java/com/gyb/domain/Account.java
@@ -0,0 +1,47 @@
+package com.gyb.domain;
+
+import java.io.Serializable;
+
+/**
+ * 实体类
+ *
+ * @author gyb
+ */
+public class Account implements Serializable {
+    private Integer id;
+    private String name;
+    private float money;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
